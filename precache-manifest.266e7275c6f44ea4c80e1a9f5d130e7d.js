ee4bf5e82138ce7496a8473b5505f1cf92782b11
Wahab Rehman <wahab_rehman1256@outlook.com>
2020-05-30 23:49:52 +0100

    Updates

---
+++ precache-manifest.266e7275c6f44ea4c80e1a9f5d130e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0ba4a41a16ad3e03c8f70310386aed",
+    "url": "/BBC-CodeTest-React/index.html"
+  },
+  {
+    "revision": "000ef82ba3f67226598d",
+    "url": "/BBC-CodeTest-React/static/css/main.f389eb40.chunk.css"
+  },
+  {
+    "revision": "0ebc3af0cea3e3218d9b",
+    "url": "/BBC-CodeTest-React/static/js/2.9ef4b339.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BBC-CodeTest-React/static/js/2.9ef4b339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000ef82ba3f67226598d",
+    "url": "/BBC-CodeTest-React/static/js/main.25ed5a6b.chunk.js"
+  },
+  {
+    "revision": "36d4acf0edf35cfa1db7",
+    "url": "/BBC-CodeTest-React/static/js/runtime-main.34e25692.js"
+  }
+]);
